7119c019219e34d49b8100d37b03d1be7e5d7635
MoustafaDarweeshDev <105270515+MoustafaDarweeshDev@users.noreply.github.com>
2022-09-11 08:19:28 +0200

    initial commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Cakery.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cakery.Data
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
