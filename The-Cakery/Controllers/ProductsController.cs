8279a959d68fa0fd924873e17bd306960e16f4ce
MoustafaDarweeshDev <moustafa.darweesh.dev@gmail.com>
2022-09-26 10:44:19 +0200

    created database added 2 class librarys

---
+++ The-Cakery/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace The_Cakery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext db;
+
+        public ProductsController(StoreContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await db.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await db.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
